a136a6db3bccffc23406d96deda359e5e1185191
Vic Kostrzewski <40337208+wikthor@users.noreply.github.com>
2018-06-17 20:42:26 +0100

    Add files via upload
    Exercise 3 from the book, with my comments alongside the lines.

---
+++ ex3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") #This prints what I'm about to do
+
+print("Hens", 25 + 30 / 6) #This counts the hens with the heading Hens printed
+print("Roosters", 100 - 25 * 3 % 4) #This counts the roosters with the heading
+
+print("Now I will count the eggs:") #This prints the next thing I'm about to do
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) #This counts the eggs
+
+print("Is it true that 3 + 2 < - 5 7?") #This prints the question
+
+print(3 + 2 < 5 - 7) #This will print the True or False answer to the above
+
+print("What is 3 + 2?", 3 + 2) #This prints the question and also the calc
+print("What is 5 - 7?", 5 - 7) #As above
+
+print("On, that's why it's False.") #Commentary on the result from line 12
+
+print("How about some more.") #Next step introduced
+
+print("Is it greater?", 5 > -2) #This prints the question and then T/F answer
+print("Is it greater or equal?", 5 >= -2) #As above
+print("Is it less or equal?", 5 <= -2) #As above
